6327567393df18c58773d35252c92538a596f892
AmbikaPathak <ambikathe001@gmail.com>
2021-09-30 11:47:04 +0530

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,12 @@
+from os import name
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.login_page,name='login'), 
+    path('Home',views.home,name='home'),
+    path('profile',views.profile,name='profile'),
+    path('logout',views.Logout,name='logout'),   
+    path('register',views.register,name='register'),
+    path('post',views.create_post,name='post'),
+    path('update',views.update_profile,name='update')
+]
